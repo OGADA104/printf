398679b2a7ba58117b080bad75a0a6ed09d579ac
Oluwasheyi <jaywoncoded@gmail.com>
2023-08-24 13:48:29 +0000

    Update: _printf to use _putchar to write

--- _printf.c
+++ _printf.c
@@ -1,146 +1,75 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdarg.h> /* for va_list and va_start */
 
 /**
- * print_binary - Print an unsigned integer in binary format.
- * @n: The unsigned integer to print.
- * @count: A pointer to the count variable to update.
- */
-void print_binary(unsigned int n, int *count)
-{
-	if (n > 1)
-		print_binary(n / 2, count);
-		putchar('0' + n % 2);
-		(*count)++;
-}
-void print_unsigned(unsigned int n, int *count, int base)
-{
-	if (n >= (unsigned int)base)
-		print_unsigned(n / base, count, base);
-		putchar("0123456789ABCDEF"[n % base]);
-		(*count)++;
-}
-
-/**
- * print_pointer - Print the hexadecimal representation of a pointer.
- * @ptr: The pointer to print.
- * @count: A pointer to the count variable to update.
- */
-void print_pointer(void *ptr, int *count)
-{
-    putchar('0');
-    putchar('x');
-    print_unsigned((unsigned long)ptr, count, 16);
-}
-
-
-/**
- * _printf - Custom printf function implementation.
- * @format: The format string.
- * @...: Additional arguments depending on format string.
+ * _printf - Produces output according to a format.
+ * @format: A character string containing format specifiers.
  *
- * Return: The number of characters printed.
+ * Return: The number of characters printed (excluding the null byte).
  */
-
-
 int _printf(const char *format, ...)
 {
-	int count = 0; /* Counter to keep track of characters printed */
-	va_list args;  /* Variable argument list */
+	int count = 0;
+	va_list args;
 
-	va_start(args, format); /* Initialize variable argument list */
-	while (*format)
+	va_start(args, format);
+
+	while (format && *format)
 	{
-		if (*format != '%')
+		if (*format != '%') /* Normal character, just print it */
 		{
-			putchar(*format); /* Print non-format characters */
+			_putchar(*format);
 			count++;
 		}
 		else
 		{
-			format++; /* Move past the '%' character */
-			switch (*format)
+			format++; /* Move past the '%' */
+
+			if (*format == 'c') /* Character */
 			{
-				case 'c':
+				char c = va_arg(args, int);
+
+				write(1, &c, 1);
+				count++;
+			}
+			else if (*format == 's') /* String */
+			{
+				char *str = va_arg(args, char *);
+
+				if (str)
 				{
-					int c = va_arg(args, int);
-					putchar(c);
-					count++;
-					break;
+					int len = 0;
+
+					while (str[len])
+					len++;
+					write(1, str, len);
+					count += len;
 				}
-				case 's':
+				else /* Handle NULL pointer */
 				{
-					char *str = va_arg(args, char *);
-					if (str)
-					{																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																					a while (*str)
-                        {
-                            putchar(*str);
-                            str++;
-                            count++;
-                        }
-                    }
-                    break;
+					write(1, "(null)", 6);
+					count += 6;
+				}
+			}
+			else if (*format == '%') /* Percentage sign */
+			{
+				write(1, "%", 1);
+				count++;
+			}
+			else /* Unknown specifier, print the '%' */
+			{
+				write(1, "%", 1);
+				count++;
+				if (*format != '\0')
+				{
+					write(1, format, 1);
+					count++;
+				}
+			}
 		}
-		case 'd':
-                case 'i':
-                {
-                    int num = va_arg(args, int);
-                    printf("%d", num);
-                    count += snprintf(NULL, 0, "%d", num);
-                    break;
-                }
-		case 'b':
-                {
-                    unsigned int num = va_arg(args, unsigned int);
-                    print_binary(num, &count);
-                    break;
-                }
-		 case 'X':
-		{
-			unsigned int num = va_arg(args, unsigned int);
-			print_unsigned(num, &count, 16);
-			break;
-		}
-		 case 'u':
-                {
-                    unsigned int num = va_arg(args, unsigned int);
-                    print_unsigned(num, &count, 10);
-                    break;
-                }
-                case 'o':
-                {
-                    unsigned int num = va_arg(args, unsigned int);
-                    printf("%o", num);
-		    print_unsigned(num, &count, 8);
-                    break;
-                }
-                case 'x':
-                {
-                    unsigned int num = va_arg(args, unsigned int);
-                    print_unsigned(num, &count, 16);
-                    break;
-                }
-		case 'p':
-                {
-                    void *ptr = va_arg(args, void *);
-                    print_pointer(ptr, &count);
-                    break;
-                }
-                case '%':
-                    putchar('%');
-                    count++;
-                    break;
-                default:
-                    putchar('%');
-                    putchar(*format);
-                    count += 2;
-            }
-        }
-        format++;
-    }
 
-    va_end(args); /* Clean up variable argument list */
-    return count;
+	format++;
+	}
+
+	va_end(args);
+	return (count);
 }
-
